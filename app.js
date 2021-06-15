72d5d02c04bc52191a5e564d5ec8fa1d063c246a
Jesse Brink <jbrink0925@gmail.com>
2021-06-15 11:37:26 -0400

    add form and add some css

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Let's get this party started!");
+
+const form = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const searchButton = document.querySelector('#searchButton');
+const removeButton = document.querySelector('#removeButton');
